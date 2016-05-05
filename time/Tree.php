b2245e3b68ea7435a45283a54a1142bc9b6ca996
leedaning <leedaning@163.com>
2016-05-05 07:34:49 +0000

    Add new file

---
+++ time/Tree.php
@@ -0,0 +1,36 @@
+<?php
+namespace Time;
+echo '<BR>Time';
+
+class Tree
+{
+
+	public function leaves($value='')
+	{
+		echo '<BR>Time leaves';
+	}
+}//end class
+
+
+namespace Time1;
+// Time1 命名空间中的PHP代码
+class Tree
+{
+
+	public function leaves($value='')
+	{
+		echo 'Time leaves 01';
+	}
+}//end class
+
+
+namespace Time2;
+// Time2 命名空间中的PHP代码
+class Tree
+{
+
+	public function leaves($value='')
+	{
+		echo 'Time leaves 02';
+	}
+}//end class
